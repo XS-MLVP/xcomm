0323b2bdfccc374a3a7433a2ded37fc441f73d82
yaozhicheng <yaozhicheng@ict.ac.cn>
2024-01-25 13:27:01 +0800

    convert bytes

--- include/xspcomm/uvm_msg.h
+++ include/xspcomm/uvm_msg.h
@@ -10,6 +10,9 @@ class uvm_msg
 public:
     uvm_msg(){}
     uvm_msg(uint8_t*addr_start, uint8_t* addr_end):data(addr_start, addr_end){}
+    uvm_msg(std::vector<uint8_t> &data):data(data){}
+    void from_bytes(std::vector<uint8_t> &data) {this->data = data;}
+    std::vector<uint8_t> as_bytes() const {return this->data;}
     uint8_t cmd;
     uint8_t resp_status;
     std::vector<uint8_t> data;
