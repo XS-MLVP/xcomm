079ae0cafb51bb9dd2dc6eda2db2f2865ee7076b
yaozhicheng <yaozhicheng@ict.ac.cn>
2024-12-25 15:46:33 +0800

    lua: test set/get bytes

--- tests/test_lua.lua
+++ tests/test_lua.lua
@@ -12,6 +12,16 @@ b:Set(0x87654321)
 print("a = ", a:AsBinaryString())
 print("b = ", b:String())
 
+-- test get/set bytes
+local vec = x.UCharVector()
+vec:push_back(1)
+vec:push_back(2)
+vec:push_back(3)
+a:Set(vec)
+print("a = ", a:AsBinaryString())
+vec = a:GetBytes()
+print("a = ", vec[0], vec[1], vec[2])
+
 clock = x.XClock(function(c) end)
 clock:StepRis(function(c) print("callback: R cycle =>", c) end)
 clock:StepFal(function(c) print("callback: F cycle =>", c) end)
