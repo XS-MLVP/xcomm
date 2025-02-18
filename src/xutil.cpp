53edb8959ada10cf869fc326deed77fb0037b372
yaozhicheng <yaozhicheng@ict.ac.cn>
2025-02-18 16:34:02 +0800

    feature: use env.XSPCOMM_LOG_LEVEL control log level

--- src/xutil.cpp
+++ src/xutil.cpp
@@ -1,9 +1,23 @@
 #include "xspcomm/xutil.h"
+#include <map>
 
 namespace xspcomm {
 LogLevel log_level = LogLevel::info;
 LogLevel get_log_level()
 {
+    std::map<std::string, LogLevel> levels;
+    levels["debug"] = LogLevel::debug;
+    levels["info"] = LogLevel::info;
+    levels["error"] = LogLevel::error;
+    levels["warn"] = LogLevel::warning;
+    auto env = getenv("XSPCOMM_LOG_LEVEL");
+    if (env){
+        auto lv = sLower(env);
+        if (levels.count(lv) > 0)
+        {
+            return levels[lv];
+        }
+    }
     return log_level;
 }
 void set_log_level(LogLevel val)
